c1c80b4c00f409f449892fb98bc92a7ed18bc85c
hjy5421 <hjy5421@naver.com>
2021-07-05 14:41:04 +0900

    http servlet request, response

--- servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -21,5 +21,23 @@ public class RequestParamServlet extends HttpServlet {
         request.getParameterNames().asIterator()
                 .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[ 단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println();
+
+        /**
+         * http://localhost:8080/request-param?username=hello&age=20&username=hello2
+         */
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames=request.getParameterValues("username");
+        for(String name : usernames){
+            System.out.println("username = "+name);
+        }
     }
 }
