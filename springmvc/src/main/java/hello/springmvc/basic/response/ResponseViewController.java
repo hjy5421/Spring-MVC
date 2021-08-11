b021f0335b8569ad8cab9bf5a34e70203fd6cf4e
hjy5421 <hjy5421@naver.com>
2021-08-11 17:55:02 +0900

    http request

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,8 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ResponseViewController {
+
+}
